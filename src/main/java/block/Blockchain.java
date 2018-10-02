208fb2a037abb5480a2a225ad2b10906bb7b046c
John-John Markstedt <c14jmt@cs.umu.se>
2018-10-02 10:09:18 +0200

    started on blockchain class

---
+++ src/main/java/block/Blockchain.java
@@ -0,0 +1,30 @@
+package block;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+
+import static java.lang.Math.toIntExact;
+
+public class Blockchain {
+
+    private Block[] blocks;
+
+    Blockchain(){
+
+    }
+
+
+    private Block createGenesisBlocK() throws NoSuchAlgorithmException {
+
+        String s = "JJ_AND_CJ_WAS_HERE_2018_10_02_UME";
+        return new Block(0, s.getBytes(), getUnixTimestamp() );
+    }
+
+    /**
+     * Needs to be casted to int to follow the Bitcoin reference implementation.
+     */
+    private static int getUnixTimestamp(){
+        return toIntExact(System.currentTimeMillis() / 1000L);
+    }
+
+}
